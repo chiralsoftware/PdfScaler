787426548c2c6d93540e298d822ac59b76348391
hh <info@chiralsoftware.com>
2021-09-02 12:34:14 -0700

    minor changes

--- PdfScaler/src/main/java/com/chiralsoftware/pdfscaler/ScaleIt.java
+++ PdfScaler/src/main/java/com/chiralsoftware/pdfscaler/ScaleIt.java
@@ -1,7 +1,7 @@
 package com.chiralsoftware.pdfscaler;
 
 import com.itextpdf.text.Document;
-import com.itextpdf.text.PageSize;
+import static com.itextpdf.text.PageSize.LETTER;
 import com.itextpdf.text.Rectangle;
 import com.itextpdf.text.pdf.PdfContentByte;
 import com.itextpdf.text.pdf.PdfImportedPage;
@@ -10,7 +10,7 @@ import com.itextpdf.text.pdf.PdfWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.PrintStream;
+import static java.lang.System.err;
 import java.util.logging.Logger;
 
 public final class ScaleIt {
@@ -31,11 +31,11 @@ public final class ScaleIt {
         for(int i = 1; i <= pdfReader.getNumberOfPages(); i++) {
 //            LOG.info("The page size of page " + i + " is: " + pdfReader.getPageSize(i));
             final Rectangle rectangle = pdfReader.getPageSize(i);
-            if(rectangle.getHeight() < PageSize.LETTER.getHeight() * 0.95) { pdfReader.close(); return false; }
-            if(rectangle.getHeight() > PageSize.LETTER.getHeight() * 1.05) { pdfReader.close(); return false; }
+            if(rectangle.getHeight() < LETTER.getHeight() * 0.95) { pdfReader.close(); return false; }
+            if(rectangle.getHeight() > LETTER.getHeight() * 1.05) { pdfReader.close(); return false; }
 
-            if(rectangle.getWidth() < PageSize.LETTER.getWidth() * 0.95)  { pdfReader.close(); return false; }
-            if(rectangle.getWidth() > PageSize.LETTER.getWidth() * 1.05)  { pdfReader.close(); return false; }
+            if(rectangle.getWidth() < LETTER.getWidth() * 0.95)  { pdfReader.close(); return false; }
+            if(rectangle.getWidth() > LETTER.getWidth() * 1.05)  { pdfReader.close(); return false; }
         }
         pdfReader.close();
         return true;
@@ -54,26 +54,24 @@ public final class ScaleIt {
             LOG.info("file: " + inputFile + " is letter sized, skipping");
             return;
         }
-//        LOG.info("I should process file: "+ inputFile);
-//        if(true) return;
         if(! (inputFile.exists() && inputFile.canRead())) {
-            System.err.println("FIle: " + inputFile + " doesn't exist or can't read");
+            err.println("FIle: " + inputFile + " doesn't exist or can't read");
             return;
         }
         final File backupFile = new File(inputFileName + ".bak");
         if(backupFile.exists()) {
-            System.err.println("Backup file: " + backupFile + " already exists");
+            err.println("Backup file: " + backupFile + " already exists");
             return;
         }
         
         final boolean renameResult = inputFile.renameTo(backupFile);
         if(! renameResult) {
-            System.err.println("Couldn't rename file: " + inputFile + " to: " + backupFile);
+            err.println("Couldn't rename file: " + inputFile + " to: " + backupFile);
             return;
         }
         
         final PdfReader pdfReader = new PdfReader(new FileInputStream(backupFile));
-        final Document document = new Document(PageSize.LETTER);
+        final Document document = new Document(LETTER);
         final PdfWriter pdfWriter = PdfWriter.getInstance(document, new FileOutputStream(inputFile));
         document.open();
         final PdfContentByte pdfContentByte =  pdfWriter.getDirectContent();
@@ -81,7 +79,7 @@ public final class ScaleIt {
 //            LOG.info("Processing page: " + i);
             document.newPage();
             final PdfImportedPage pdfImportedPage = pdfWriter.getImportedPage(pdfReader, i);
-            final float scale = PageSize.LETTER.getHeight() / pdfImportedPage.getHeight();
+            final float scale = LETTER.getHeight() / pdfImportedPage.getHeight();
             pdfContentByte.addTemplate(pdfImportedPage, scale, 0,0, scale, 0,0);
         }
         document.close();
